bfa6c8d1cc9a9d44b002b941e1effb320939c935
degznn <diego.condori.m@tecsup.edu.pe>
2026-06-29 11:05:35 -0500

    Forzar HTTPS en produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,23 +7,22 @@ use App\Models\Estudiante;
 use App\Policies\EntrevistaPolicy;
 use App\Policies\EstudiantePolicy;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Gate::policy(Estudiante::class, EstudiantePolicy::class);
         Gate::policy(Entrevista::class, EntrevistaPolicy::class);
 
